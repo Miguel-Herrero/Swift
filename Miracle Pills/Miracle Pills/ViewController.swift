ef31707689a496b353bc70daa5c2cfed93a4a5f5
Miguel Herrero <miguel.herrerobaena@gmail.com>
2016-11-25 23:25:55 +0100

    Simplified access to UIViews

--- Miracle Pills/Miracle Pills/ViewController.swift
+++ Miracle Pills/Miracle Pills/ViewController.swift
@@ -10,24 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var pillImage: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var subtitleLabel: UILabel!
-    @IBOutlet weak var dividerView: UIView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var addressTextField: UITextField!
-    @IBOutlet weak var cityLabel: UILabel!
-    @IBOutlet weak var cityTextField: UITextField!
-    @IBOutlet weak var stateLabel: UILabel!
     @IBOutlet weak var statePicker: UIPickerView!
     @IBOutlet weak var statePickerButton: UIButton!
     @IBOutlet weak var countryLabel: UILabel!
     @IBOutlet weak var countryTextField: UITextField!
     @IBOutlet weak var zipLabel: UILabel!
     @IBOutlet weak var zipTextField: UITextField!
-    @IBOutlet weak var buyButton: UIButton!
     @IBOutlet weak var successImage: UIImageView!
     
     let states = [
@@ -66,26 +54,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buyButtonPressed(_ sender: Any) {
-        pillImage.isHidden = true
-        titleLabel.isHidden = true
-        subtitleLabel.isHidden = true
-        dividerView.isHidden = true
-        nameLabel.isHidden = true
-        nameTextField.isHidden = true
-        addressLabel.isHidden = true
-        addressTextField.isHidden = true
-        cityLabel.isHidden = true
-        cityTextField.isHidden = true
-        stateLabel.isHidden = true
-        statePicker.isHidden = true
-        statePickerButton.isHidden = true
-        countryLabel.isHidden = true
-        countryTextField.isHidden = true
-        zipLabel.isHidden = true
-        zipTextField.isHidden = true
-        buyButton.isHidden = true
+        for view in self.view.subviews {
+            view.isHidden = true
+        }
         
-        successImage.isHidden = false
+        //Access to Success Imageview without declaring it as an IBOutlet
+        self.view.viewWithTag(20)?.isHidden = false
     }
     
 }
