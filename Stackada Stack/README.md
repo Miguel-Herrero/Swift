37ff054a018b0809a56bc4dad7f9c4b575b6db1e
Miguel Herrero <miguel.herrerobaena@gmail.com>
2016-11-26 18:02:53 +0100

    devslopes-ios10 4.48 (Stackviews)

---
+++ Stackada Stack/README.md
@@ -0,0 +1,9 @@
+# Stackada Stack
+
+Based on Udemy's course [“iOS 10 & Swift 3: From Beginner to Paid Professional”](https://www.udemy.com/devslopes-ios10/learn/v4/t/lecture/5428772)
+
+Simple UI to learn Stackviews:
+
+- Horizontal and vertical Stackviews.
+- Variations of Stackview orientations for portrait and horizontal devices.
+- Content hugging priority and Content compression resistance priority instead of constraints for buttons.
---
+++ Stackada Stack/Stackada Stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stackada Stack
+//
+//  Created by Miguel Herrero on 26/11/16.
+//  Copyright © 2016 Miguel Herrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
