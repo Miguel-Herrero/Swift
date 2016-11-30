4bf1769c304de684bb425fde2bcbd9c8dcb63262
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2016-11-30 17:05:29 +0100

    devslopes-ios10 5.73 (IBOutlets)

---
+++ Dream Lister/Dream Lister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  Dream Lister
+//
+//  Created by Miguel Herrero on 30/11/16.
+//  Copyright © 2016 Miguel Herrero. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
