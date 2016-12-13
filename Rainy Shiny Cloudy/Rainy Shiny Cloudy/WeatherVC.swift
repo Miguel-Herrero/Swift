69e73578693de8131a9efc92de92ff4bc7b00ef2
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2016-12-13 16:35:09 +0100

    devslopes-ios10 6.90 (UI and Outlets)

---
+++ Rainy Shiny Cloudy/Rainy Shiny Cloudy/WeatherVC.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherVC.swift
+//  Rainy Shiny Cloudy
+//
+//  Created by Miguel Herrero on 13/12/16.
+//  Copyright © 2016 Miguel Herrero. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentTempImage: UIImageView!
+    @IBOutlet weak var currentTempTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
