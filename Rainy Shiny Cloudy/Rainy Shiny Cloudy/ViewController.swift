cdf83ff5546d068167f2e3f40db6bb6406bc0169
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2016-12-13 10:14:56 +0100

    Base XCode project with Alamofire pod

---
+++ Rainy Shiny Cloudy/Rainy Shiny Cloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rainy Shiny Cloudy
+//
+//  Created by Miguel Herrero on 13/12/16.
+//  Copyright © 2016 Miguel Herrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
