64dbe096119e705e3216e6d28dd0948bf920dca0
Miguel Herrero <miguel.herrerobaena@gmail.com>
2016-12-17 11:25:15 +0100

    devslopes-ios10 6.112 (iboutlets)

--- Pokedex/Pokedex/PokemonDetailVC.swift
+++ Pokedex/Pokedex/PokemonDetailVC.swift
@@ -11,6 +11,17 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexIDLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
 
     var pokemon: Pokemon!
 
@@ -21,4 +32,8 @@ class PokemonDetailVC: UIViewController {
 
     }
 
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
