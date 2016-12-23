c4460f9fe7c9b8ac3d13086b301dd1aa2c12f8cb
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2016-12-23 13:18:15 +0100

    devslopes-ios10 10.138 (saving pokemon to GPS location)

--- Poke-Search/Poke-Search/ViewController.swift
+++ Poke-Search/Poke-Search/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import MapKit
+import GeoFire
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
@@ -15,11 +17,16 @@ class ViewController: UIViewController {
     
     fileprivate let locationManager = CLLocationManager()
     var mapHasCenteredOnce = false
+    var geoFire: GeoFire!
+    var geoFireRef: FIRDatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.userTrackingMode = .follow
+        
+        geoFireRef = FIRDatabase.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireRef)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,8 +39,29 @@ class ViewController: UIViewController {
         
         locationAuthStatus()
     }
+    
+    func createSighting(for location: CLLocation, with pokemonId: Int) {
+        geoFire.setLocation(location, forKey: "\(pokemonId)")
+    }
+    
+    func showSightingsOnMap(location: CLLocation) {
+        let circleQuery = geoFire!.query(at: location, withRadius: 2)
+        
+        _ = circleQuery?.observe(.keyEntered, with: { (key, location) in
+            
+            if let key = key, let location = location {
+                let annotation = PokeAnnotation(coordinate: location.coordinate, pokemonNumber: Int(key)!)
+                self.mapView.addAnnotation(annotation)
+            }
+        })
+    }
 
     @IBAction func spotRandomPokemon(_ sender: UIButton) {
+        
+        let location = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        
+        let rand = arc4random_uniform(151) + 1
+        createSighting(for: location, with: Int(rand))
     }
 
 }
