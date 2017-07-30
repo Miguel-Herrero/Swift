f7b18d4824b706ba2ecd5b3981ea563ee18e06e4
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2017-07-31 00:08:31 +0200

    Configuring new project

---
+++ WhatsUp/WhatsUp/Scene.swift
@@ -0,0 +1,24 @@
+//
+//  Scene.swift
+//  WhatsUp
+//
+//  Created by Miguel Herrero on 31/7/17.
+//  Copyright © 2017 Miguel Herrero. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
