e82fe6114006e64a398dd59497ff23017ab96df6
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2019-06-13 23:33:50 +0200

    New project for TDD

---
+++ Testing Swift/TDD_Project/TDD_ProjectTests/TDD_ProjectTests.swift
@@ -0,0 +1,14 @@
+//
+//  TDD_ProjectTests.swift
+//  TDD_ProjectTests
+//
+//  Created by Miguel Herrero on 13/06/2019.
+//  Copyright © 2019 Miguel Herrero. All rights reserved.
+//
+
+import XCTest
+@testable import TDD_Project
+
+class TDD_ProjectTests: XCTestCase {
+
+}
